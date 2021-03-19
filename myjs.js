f840d3db88a4bcab8d430d65c7c6ac1ff862f1e4
cescobarete <46694783+cescobarete@users.noreply.github.com>
2021-03-19 16:38:02 -0500

    Hwllo

---
+++ myjs.js
@@ -0,0 +1,17 @@
+//Roll dice for a game, automatically make a number between 1-6
+function RollDie() {
+    document.getElementById("Dice").value = Math.floor(Math.random()*6) + 1;
+}
+//New function for each dice
+function RollDie2() {
+    document.getElementById("Dice2").value = Math.floor(Math.random()*6) + 1;
+}
+function RollDie3() {
+    document.getElementById("Dice3").value = Math.floor(Math.random()*6) + 1;
+}
+function RollDie4() {
+    document.getElementById("Dice4").value = Math.floor(Math.random()*6) + 1;
+}
+function RollDie5() {
+    document.getElementById("Dice5").value = Math.floor(Math.random()*6) + 1;
+}
